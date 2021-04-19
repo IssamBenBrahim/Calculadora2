d4b7b351f7b1d0d3e37f67ff892f022f8bfe50c7
Issam <issambenbrahim2001@gmail.com>
2021-04-19 19:32:25 +0200

    Seguimiento del anterior repositoio Calculadora y del proyecto CalculadoraApp

---
+++ CalculadoraApp2/CalculadoraApp2/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalculadoraApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_Clicked(object sender, EventArgs e)
+        {
+            Resultado.Text = Resultado.Text + ((Button)sender).Text;
+        }
+
+        private void BotonDividir_Clicked(object sender, EventArgs e)
+        {
+
+            if (!Resultado.Text.Equals(""))
+            {
+                string signo_operacion = Resultado.Text.Substring(Resultado.Text.Length - 1);
+
+                if (!(signo_operacion.Equals("/") || signo_operacion.Equals("x") ||
+                    signo_operacion.Equals("-") || signo_operacion.Equals("+")))
+                {
+                    Resultado.Text = Resultado.Text + "/";
+                }
+            }
+        }
+
+        private void BotonMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            if (!Resultado.Text.Equals(""))
+            {
+                string signo_operacion = Resultado.Text.Substring(Resultado.Text.Length - 1);
+
+                if (!(signo_operacion.Equals("/") || signo_operacion.Equals("x") ||
+                    signo_operacion.Equals("-") || signo_operacion.Equals("+")))
+                {
+                    Resultado.Text = Resultado.Text + "x";
+                }
+            }
+        }
+
+        private void BotonRestar_Clicked(object sender, EventArgs e)
+        {
+            if (!Resultado.Text.Equals(""))
+            {
+                string signo_operacion = Resultado.Text.Substring(Resultado.Text.Length - 1);
+
+                if (!(signo_operacion.Equals("/") || signo_operacion.Equals("x") ||
+                    signo_operacion.Equals("-") || signo_operacion.Equals("+")))
+                {
+                    Resultado.Text = Resultado.Text + "-";
+                }
+            }
+        }
+
+        private void BotonSumar_Clicked(object sender, EventArgs e)
+        {
+            if (!Resultado.Text.Equals(""))
+            {
+                string signo_operacion = Resultado.Text.Substring(Resultado.Text.Length - 1);
+
+                if (!(signo_operacion.Equals("/") || signo_operacion.Equals("x") ||
+                    signo_operacion.Equals("-") || signo_operacion.Equals("+")))
+                {
+                    Resultado.Text = Resultado.Text + "+";
+                }
+            }
+        }
+
+        private void BotonBorrar_Clicked(object sender, EventArgs e)
+        {
+            Resultado.Text = "";
+        }
+
+        private void BotonIgual_Clicked(object sender, EventArgs e)
+        {
+            Historial.Children.Add(new Label() { Text = Resultado.Text });
+
+        }
+
+    }
+}
